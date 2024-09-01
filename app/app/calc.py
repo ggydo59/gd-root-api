868f0ec4318a5f65cbd3ac240ea1404477dcc348
민경도 <gydo@mingyeongdoui-noteubug.local>
2024-09-01 19:53:35 +0900

    chore: add to Postgresql and dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,18 @@
+"""
+Calculator functions
+"""
+
+
+
+def add(x, y):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+    """
+    return x+y
+
+def substract(x, y):
+    """"""
+    return x-y
